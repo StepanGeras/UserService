8143b69cb2c7e659214f777a691d33d785ec04d8
Shifernik <m.Disai@mail.ru>
2024-10-28 09:32:55 +0300

    Kube

---
+++ src/main/java/org/example/userservice/kafka/ProducerService.java
@@ -0,0 +1,22 @@
+package org.example.userservice.kafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProducerService {
+
+    private static final String TOPIC = "my-topic";
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public ProducerService(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String message) {
+        kafkaTemplate.send(TOPIC, message);
+    }
+}
