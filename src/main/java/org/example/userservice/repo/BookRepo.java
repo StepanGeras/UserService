e5997170592601a10b53040afb9c1100acfbdaef
Shifernik <m.Disai@mail.ru>
2024-12-06 18:10:52 +0300

    fix №3

--- src/main/java/org/example/userservice/repo/BookRepo.java
+++ src/main/java/org/example/userservice/repo/BookRepo.java
@@ -4,11 +4,13 @@ import org.example.userservice.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookRepo extends JpaRepository<Book, Long> {
 
     Book findBookById(Long id);
-
+    List<Book> findAllByUserId(Long id);
 }
 
 
